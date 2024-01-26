5a2c341d920f624aa943a4de4edd62ab9dce29be
ArunR <you@example.com>
2024-01-26 16:45:41 +0530

    changes

--- App.js
+++ App.js
@@ -12,29 +12,40 @@ import React from "react";
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
+import { Provider } from "react-redux";
+import { Store } from "./src/Redux/Store/Store";
+
 import LogInSignUp from './src/Components/LogInSignUp'
 import Index from "./src/Components/Index";
 import UserProfileCreation from "./src/Components/UserProfileCreation";
-import LogIn from "./src/Components/LogIn";
-import { Provider } from "react-redux";
-import SignUp from "./src/Components/SignUp";
-import { Store } from "./src/Redux/Store/Store";
+import LogIn from "./src/Components/Registration/LogIn";
+import SignUp from "./src/Components/Registration/SignUp";
+import ForgotPassword from "./src/Components/Registration/ForgotPassword";
+import basicStrings from './src/Strings/basics.json'
+import { View } from "react-native";
+import appColors from './src/Others/appColors.json'
 
 const Stack = createStackNavigator();
 
 export default function App() {
 
   return (
-    <Provider store={Store}>
+    <View style={{flex:1}}>
+      <Provider store={Store}>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="LogIn">
-          <Stack.Screen name="Index" component={Index} options={{ headerShown: false }} />
-          <Stack.Screen name="LogIn" component={LogIn} />
-          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="Index" component={Index} options={{ headerShown: false }}/>
+          <Stack.Screen name="LogIn" component={LogIn} options={{ headerTitle: basicStrings.appName, headerStyle: { backgroundColor: appColors.basicRed}, headerTintColor:'white' }} />
+          <Stack.Screen name="SignUp" component={SignUp} options={{ headerTitle: 'Create New Account', headerStyle: { backgroundColor: appColors.basicRed } , headerTintColor:'white'}} />
+          <Stack.Screen name="Forgot Password" component={ForgotPassword} options={{ headerTitle: 'Reset Password', headerStyle: { backgroundColor: appColors.basicRed }, headerTintColor:'white' }} />
           <Stack.Screen name="UserProfileCreation" component={UserProfileCreation} options={{ headerShown: false }} />
         </Stack.Navigator>
       </NavigationContainer>
-    </Provider>
+  </Provider>
+    </View>
+    
+
+
   );
 }
 
